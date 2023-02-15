f4502a9ed998a24212aabc5e3f02e66a0dc2a373
abdulohab90208533 <abdulohab90208533@gmail.com>
2023-02-15 17:05:17 +0600

    get all html element and create a function to show the massage in todo app

--- todo.js
+++ todo.js
@@ -1,1 +1,14 @@
- 
+ const todoForm = document.querySelector("form");
+ const todoInput = document.querySelector("input");
+ const todoLists = document.querySelector("#lists");
+ const massageElement = document.querySelector("#massage");
+
+ const showMassage = (text, status) => {
+   massageElement.textContent = text;
+   massageElement.classList.add(`bg-${status}`);
+
+   setTimeout(() => {
+     massageElement.textContent = "";
+     massageElement.classList.remove(`bg-${status}`);
+   }, 1000);
+ };
